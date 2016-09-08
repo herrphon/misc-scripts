9fba2465a623c2f3d7a9b225a195e5b95a37ed96
herrphon <herrphon@gmail.com>
2016-09-08 11:00:10 +0200

    Update show-field-with-config.groovy

--- jira/show-field-with-config.groovy
+++ jira/show-field-with-config.groovy
@@ -3,41 +3,54 @@ import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.fields.config.FieldConfig;
 import com.atlassian.jira.issue.customfields.CustomFieldUtils;
 import com.atlassian.jira.issue.customfields.impl.SelectCFType;
+import com.atlassian.jira.issue.CustomFieldManager
+import com.atlassian.jira.issue.customfields.manager.OptionsManager
 
 
-def customFieldManager = ComponentAccessor.getCustomFieldManager();
+class YamlGenerator {
+    private final CustomFieldManager customFieldManager;
+    private final OptionsManager optionsManager;
 
+    public YamlGenerator(CustomFieldManager customFieldManager, OptionsManager optionsManager) {
+		this.customFieldManager = customFieldManager;
+        this.optionsManager = optionsManager;
+	}
 
-public FieldConfig getFieldConfig(CustomField customField) {
-    def contexts = CustomFieldUtils.buildJiraIssueContexts(true, null, null);
-    def context = contexts.get(0);
-    FieldConfig other = customField.getConfigurationSchemes().listIterator().next().getOneAndOnlyConfig();
-    FieldConfig global = customField.getRelevantConfig(context);
-    return (global != null ? global : other);
-}
+    public String getCustomFieldValue(String customFieldName) {
+        Collection<CustomField> customFields = customFieldManager.getCustomFieldObjectsByName(customFieldName);
+        CustomField customField = customFields.first();
 
+        StringBuilder result = new StringBuilder();
+        result.append("---\ncustomfields:\n")
+        result.append("- name: " + customField.getName() + "\n")
 
+        if (customField.getCustomFieldType() instanceof SelectCFType) {
+            def customFieldConfig = getFieldConfig(customField);
+            def options = optionsManager.getOptions(customFieldConfig);
 
-Collection<CustomField> customFieldList = customFieldManager.getCustomFieldObjectsByName("blubb");
+            result.append("  type: SelectList \n  values: \n")
 
-def customField = customFieldList.first();
-
-StringBuilder result = new StringBuilder();
-result.append("---\ncustomfields:\n")
-result.append("- name: " + customField.getName() + "\n")
-
-if (customField.getCustomFieldType() instanceof SelectCFType) {
-    def customFieldConfig = getFieldConfig(customField);
-    def optionsManager = ComponentAccessor.getOptionsManager();
-    def options = optionsManager.getOptions(customFieldConfig);
-
-	result.append("  type: SelectList \n" + 
-                  "  values: \n")
-
-    options.each { option ->
-        result.append("  - " + option.toString() + "\n")
+            options.each { option ->
+                result.append("  - " + option.toString() + "\n")
+            }
+            return "<pre>" + result + "</pre>"
+        }
+        return customField.getName() + " " + customField.getCustomFieldType().getClass()
     }
-    return "<pre>" + result + "</pre>"
+    
+    private FieldConfig getFieldConfig(CustomField customField) {
+        def contexts = CustomFieldUtils.buildJiraIssueContexts(true, null, null);
+        def context = contexts.get(0);
+        FieldConfig other = customField.getConfigurationSchemes().listIterator().next().getOneAndOnlyConfig();
+        FieldConfig global = customField.getRelevantConfig(context);
+        return (global != null ? global : other);
+    }
+
 }
 
-return customField.getName() + " " + customField.getCustomFieldType().getClass()
+        
+CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager();
+OptionsManager optionsManager = ComponentAccessor.getOptionsManager();
+
+YamlGenerator yamlGenerator = new YamlGenerator(customFieldManager, optionsManager)
+yamlGenerator.getCustomFieldValue("HW Part-Number")

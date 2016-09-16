9b86c28769472a3d65defd84d86015bc44f71db9
herrphon <herrphon@gmail.com>
2016-09-16 13:32:35 +0200

    Update show-field-with-config.groovy

--- jira/show-field-with-config.groovy
+++ jira/show-field-with-config.groovy
@@ -3,8 +3,11 @@ import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.fields.config.FieldConfig;
 import com.atlassian.jira.issue.customfields.CustomFieldUtils;
 import com.atlassian.jira.issue.customfields.impl.SelectCFType;
-import com.atlassian.jira.issue.CustomFieldManager
-import com.atlassian.jira.issue.customfields.manager.OptionsManager
+import com.atlassian.jira.issue.customfields.impl.RenderableTextCFType;
+import com.atlassian.jira.issue.customfields.impl.TextAreaCFType;
+import com.atlassian.jira.issue.customfields.impl.DateCFType;
+import com.atlassian.jira.issue.CustomFieldManager;
+import com.atlassian.jira.issue.customfields.manager.OptionsManager;
 
 
 class YamlGenerator {
@@ -16,12 +19,22 @@ class YamlGenerator {
         this.optionsManager = optionsManager;
 	}
 
-    public String getCustomFieldValue(String customFieldName) {
+    public String getCustomFieldValues(customFieldNames) {       
+        String result = "---\ncustomfields:\n"
+
+		customFieldNames.each { 
+    		result += getCustomFieldValue(it)
+		}
+        
+        
+		return "<pre>" + result + "</pre>"
+    }
+    
+    private String getCustomFieldValue(String customFieldName) {
         Collection<CustomField> customFields = customFieldManager.getCustomFieldObjectsByName(customFieldName);
         CustomField customField = customFields.first();
 
         StringBuilder result = new StringBuilder();
-        result.append("---\ncustomfields:\n")
         result.append("- name: " + customField.getName() + "\n")
 
         if (customField.getCustomFieldType() instanceof SelectCFType) {
@@ -33,7 +46,19 @@ class YamlGenerator {
             options.each { option ->
                 result.append("  - " + option.toString() + "\n")
             }
-            return "<pre>" + result + "</pre>"
+            return result + "\n"
+        }
+        if (customField.getCustomFieldType() instanceof RenderableTextCFType) {
+            result.append("  type: TextField \n\n")
+        	return result
+        }
+        if (customField.getCustomFieldType() instanceof TextAreaCFType) {
+            result.append("  type: TextArea \n\n")
+        	return result
+        }
+        if (customField.getCustomFieldType() instanceof DateCFType) {
+            result.append("  type: DatePicker \n\n")
+        	return result
         }
         return customField.getName() + " " + customField.getCustomFieldType().getClass()
     }
@@ -53,4 +78,8 @@ CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager(
 OptionsManager optionsManager = ComponentAccessor.getOptionsManager();
 
 YamlGenerator yamlGenerator = new YamlGenerator(customFieldManager, optionsManager)
-yamlGenerator.getCustomFieldValue("HW Part-Number")
+
+def fields = ['field 1', 'field 2']
+
+yamlGenerator.getCustomFieldValues(fields)
+
